5b6bc8c6863d7a990463a9db8cfca168cb1022fa
wzmli <wzmichael.li@gmail.com>
2026-06-23 10:30:11 -0400

    Autocommit

--- SEIRDB_pps_plots.R
+++ SEIRDB_pps_plots.R
@@ -3,6 +3,7 @@ library(bbmle)
 library(tidyverse)
 library(ggplot2);theme_set(theme_bw())
 library(shellpipes)
+startGraphics(width=6,height=4)
 
 
 firstdate <- as.Date("2026-03-01")
@@ -16,6 +17,12 @@ simdf <- (rdsRead("sims")
 dat <- (rdsRead("clean")
 	|> select(date, newIc, newDc, cumIc = confirmed_cases, cumDc = confirmed_death)
 	|> pivot_longer(-date,names_to="matrix",values_to = "value")
+	|> mutate(report_type = matrix
+		, report_type = ifelse(report_type == "newIc", "Daily new cases", report_type)
+		, report_type = ifelse(report_type == "newDc", "Daily new death", report_type)
+		, report_type = ifelse(report_type == "cumIc", "Cumulative cases", report_type)
+		, report_type = ifelse(report_type == "cumDc", "Cumulative death", report_type)
+	)
 )
 
 simdf$value <- pmax(rnorm(mean=simdf$value,sd=8,n=length(simdf$value)),0)
@@ -52,6 +59,29 @@ print(gg2
 	+ xlim(as.Date(c("2026-05-01","2026-06-30")))
 )
 
+simdf3 <- (simdf2
+	|> mutate(report_type = matrix
+		, report_type = ifelse(report_type == "newIc", "Daily new cases", report_type)
+		, report_type = ifelse(report_type == "newDc", "Daily new death", report_type)
+		, report_type = ifelse(report_type == "cumIc", "Cumulative cases", report_type)
+		, report_type = ifelse(report_type == "cumDc", "Cumulative death", report_type)
+	)
+	|> filter(date <= as.Date("2026-07-01"))
+	|> filter(report_type %in% c("Cumulative cases","Cumulative death","Daily new cases", "Daily new death"))
+)
+
+
+gg3 <- (ggplot(simdf3, aes(date,med))
+	+ geom_line()
+	+ geom_ribbon(aes(ymin=lwr,ymax=upr),alpha=0.2)
+	+ facet_wrap(~report_type,scale="free")
+	+ geom_point(data=dat,aes(date,value),color="red",size=0.8)
+	+ xlim(as.Date(c("2026-05-01","2026-06-30")))
+	+ theme(legend.position="bottom")
+)
+
+print(gg3)
+
 outdat <- (simdf2
 	|> filter(matrix %in% c("newIc","Incidence","cumIc"))
 	|> filter(date < as.Date("2026-07-31"))
@@ -61,3 +91,5 @@ outdat <- (simdf2
 
 print(outdat,n=Inf)
 csvSave(outdat)
+
+
